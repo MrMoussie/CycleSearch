979258530dccaa67957eead48b92f21227a55157
s2536528 <i.kepka@student.utwente.nl>
2022-10-20 14:53:25 +0200

    Added changes to reverted branch, gonna make a new working one out of it.

--- app/src/main/java/com/example/cyclesearch/MapsActivity.java
+++ app/src/main/java/com/example/cyclesearch/MapsActivity.java
@@ -1,34 +1,22 @@
 package com.example.cyclesearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.Manifest;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 
@@ -57,9 +45,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private AppBarConfiguration appBarConfiguration;
     private SensorManager sensorManager;
-    private Sensor sensor;
     private MySensor mySensor;
-    private File file = new File( Environment.getExternalStorageDirectory().getPath() + "/Documents/test.csv");
+    private final File file = new File( Environment.getExternalStorageDirectory().getPath() + "/Documents/test.csv");
     private FileWriter outputfile;
     private CSVWriter writer;
     private SupportMapFragment mapFragment;
@@ -70,11 +57,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Button button5;
     private Button button6;
     private Button button7;
-    private FrameLayout frame1;
     private ConstraintLayout frame2;
     private SensorEventListener sensorListener;
     private SensorActivity sensorActivity;
-    private int val;
     private boolean init = false;
     private Excel excel;
 
@@ -84,47 +69,44 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        button1 = (Button) findViewById(R.id.button2);
+        button1 = findViewById(R.id.button2);
         button1.setOnClickListener(this);
-        button2 = (Button) findViewById(R.id.button_second);
+        button2 = findViewById(R.id.button_second);
         button2.setOnClickListener(this);
-        button3 = (Button) findViewById(R.id.button_start);
+        button3 = findViewById(R.id.button_start);
         button3.setOnClickListener(this);
-        button4 = (Button) findViewById(R.id.button_stop);
+        button4 = findViewById(R.id.button_stop);
         button4.setOnClickListener(this);
-        button5 = (Button) findViewById(R.id.button_reset);
+        button5 = findViewById(R.id.button_reset);
         button5.setOnClickListener(this);
-        button6 = (Button) findViewById(R.id.activityButton);
+        button6 = findViewById(R.id.activityButton);
         button6.setOnClickListener(this);
-        button7 = (Button) findViewById(R.id.buttonBack);
+        button7 = findViewById(R.id.buttonBack);
         button7.setOnClickListener(this);
 
         frame2 = findViewById(R.id.ConstraintLayout);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        RadioGroup radioGroup =((RadioGroup) findViewById(R.id.radioGroup));
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.bikingButton && mySensor != null){
-                    System.out.println("You are now biking");
-                    mySensor.setActivity("Biking");
-                }
-                else if(checkedId == R.id.walkingButton && mySensor != null){
-                    System.out.println("You are now walking");
-                    mySensor.setActivity("Walking");
-                }
-                else if(checkedId == R.id.standingButton && mySensor != null){
-                    System.out.println("You are standing");
-                    mySensor.setActivity("Standing");
-                }
-                else if(checkedId == R.id.sittingButton && mySensor != null){
-                    System.out.println("You are now sitting");
-                    mySensor.setActivity("Sitting");
-                }
-
+        RadioGroup radioGroup =( findViewById(R.id.radioGroup));
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if(checkedId == R.id.bikingButton && mySensor != null){
+                System.out.println("You are now biking");
+                mySensor.setActivity("Biking");
             }
+            else if(checkedId == R.id.walkingButton && mySensor != null){
+                System.out.println("You are now walking");
+                mySensor.setActivity("Walking");
+            }
+            else if(checkedId == R.id.standingButton && mySensor != null){
+                System.out.println("You are standing");
+                mySensor.setActivity("Standing");
+            }
+            else if(checkedId == R.id.sittingButton && mySensor != null){
+                System.out.println("You are now sitting");
+                mySensor.setActivity("Sitting");
+            }
+
         });
 
         Dexter.withContext(getApplicationContext())
@@ -139,23 +121,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         if (multiplePermissionsReport.areAllPermissionsGranted()) {
                             System.out.println("[SYSTEM] PERMISSION GRANTED!");
                             System.out.println(Environment.getExternalStorageDirectory().getPath());
-
-//                            // Gotta fix the permissions for writing and reading and opening and creating the file
-//                            new Handler().postDelayed(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    // on below line we are
-//                                    // creating a new intent
-//                                    Intent i = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-//                                    i.addCategory(Intent.CATEGORY_OPENABLE);
-//                                    i.setType("application/csv");
-//                                    i.putExtra(Intent.EXTRA_TITLE, "test.csv");
-//                                    startActivity(i);
-//
-//                                    // on the below line we are finishing
-//                                    // our current activity.
-//                                }
-//                            }, 3000);
                         }
                     }
 
@@ -185,7 +150,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             writer.writeNext(header);
 
             mySensor = new MySensor();
-            sensorListener = new SensorActivity(mySensor, this, file, outputfile, writer);
+            sensorListener = new SensorActivity(mySensor, writer);
             sensorActivity = (SensorActivity) sensorListener;
             excel = sensorActivity.getExcel();
             sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 200000);
@@ -219,16 +184,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-        ImageView turtle = null;
+        ImageView turtle;
         switch(view.getId()) {
             case R.id.button2:
                 FragmentManager fm = getSupportFragmentManager();
@@ -244,24 +210,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
                 break;
             case R.id.button_second:
-                button2 = (Button) findViewById(R.id.button_second);
+                button2 = findViewById(R.id.button_second);
                 button2.setOnClickListener(this);
                 findViewById(R.id.includeMain).setVisibility(View.VISIBLE);
                 frame2.setVisibility(View.GONE);
                 break;
             case R.id.button_start:
-                if (init == false) {
+                if (!init) {
                     System.out.println("Sensors ON and system initialized");
                     init();
-                    turtle = (ImageView) findViewById(R.id.turtleWantsToSpin);
+                    turtle = findViewById(R.id.turtleWantsToSpin);
                     turtle.animate().scaleX(.5f);
                     turtle.animate().scaleY(.5f);
-                   // turtle.animate().rotation(360f);
                     System.out.println("Should be rotating");
                 } else {
                     System.out.println("Sensors ON");
                     sensorON();
-                    turtle = (ImageView) findViewById(R.id.turtleWantsToSpin);
+                    turtle = findViewById(R.id.turtleWantsToSpin);
                     turtle.animate().scaleX(.5f);
                     turtle.animate().scaleY(.5f);
                 }
@@ -272,17 +237,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     break;
                 }
                 //Animation for reset
-                // Used for the purpose of resetting the animation
-
-                turtle = (ImageView) findViewById(R.id.turtleWantsToSpin);
-                //Animation for reset
+                turtle = findViewById(R.id.turtleWantsToSpin);
                 turtle.animate().rotationXBy(360f);
                 System.out.println("System RESET");
                 file.delete();
                 sensorOFF();
                 break;
             case R.id.button_stop:
-                turtle = (ImageView) findViewById(R.id.turtleWantsToSpin);
+                turtle = findViewById(R.id.turtleWantsToSpin);
                 turtle.animate().scaleX(1f);
                 turtle.animate().scaleY(1f);
                 turtle.animate().rotationXBy(360f);
@@ -309,33 +271,4 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 break;
         }
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.myMapView);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
 }
